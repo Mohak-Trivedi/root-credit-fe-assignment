909dac1d6958900cebecc8275493d7e4fdf7aea1
Mohak-Trivedi <trivedimohak@gmail.com>
2026-06-03 18:25:30 +0530

    chore: initialize project and install dependencies

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-2xl font-bold">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
